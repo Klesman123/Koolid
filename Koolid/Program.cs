986be576b18f975f64ed346e161bf6619f1038bd
opilane <opilane@HP-CZC0359NN8>
2023-02-21 09:06:57 +0200

    Add project files.

---
+++ Koolid/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Koolid
+{
+    internal class Koolid
+    {
+        static void Main(string[] args)
+        {
+            var domains = new Dictionary<string, string>
+            {
+                {"TTHK", "Tallinna Tööstushariduskeskus"},
+                {"TMG", "Tallinna Mustamäe Gümnaasium"},
+                {"TSG", "Tallinna Saksagümnaasium"},
+            };
+            int i = 1;
+            foreach (var pair in domains)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} . {i}");
+                i++;
+            }
+        }
+    }
+}
